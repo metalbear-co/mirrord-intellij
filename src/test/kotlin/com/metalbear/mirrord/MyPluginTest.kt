2453da4fd5363cad3f597b3c816cc6dd64469878
infiniteregrets <aroram18@mcmaster.ca>
2023-07-07 10:39:44 -0400

    try

--- src/test/kotlin/com/metalbear/mirrord/MyPluginTest.kt
+++ src/test/kotlin/com/metalbear/mirrord/MyPluginTest.kt
@@ -99,32 +99,32 @@ internal class MirrordPluginTest {
 //                }
 
                 // Note: Press Ctrl + Shift + N on Windows/Linux, ⌘ + ⇧ + O on macOS to invoke the Navigate to file pop-up.
-//                dumbAware {
-//                    keyboard {
-//                        if (remoteRobot.isMac()) {
-//                            hotKey(VK_SHIFT, VK_META, VK_O)
-//                        } else {
-//                            hotKey(VK_SHIFT, VK_CONTROL, VK_N)
-//                        }
-//                        enterText("app.py")
-//                        enter()
-//                    }
-//
-//
-//                    editorTabs {
-//                        waitFor {
-//                            isFileOpened("app.py")
-//                        }
-//                    }
-//                }
-
-
-                with(projectViewTree) {
-                    waitFor(ofSeconds(30)) {
-                        hasText("app.py")
+                dumbAware {
+                    keyboard {
+                        if (remoteRobot.isMac()) {
+                            hotKey(VK_SHIFT, VK_META, VK_O)
+                        } else {
+                            hotKey(VK_SHIFT, VK_CONTROL, VK_N)
+                        }
+                        enterText("app.py")
+                        enter()
                     }
-                    findText("app.py").doubleClick()
+//
+//
+                    editorTabs {
+                        waitFor {
+                            isFileOpened("app.py")
+                        }
+                    }
                 }
+
+
+//                with(projectViewTree) {
+//                    waitFor(ofSeconds(30)) {
+//                        hasText("app.py")
+//                    }
+//                    findText("app.py").doubleClick()
+//                }
 //                if (!poetryDialog) {
 //                    step("Set up Poetry Environment") {
 //                        fileIntention {
