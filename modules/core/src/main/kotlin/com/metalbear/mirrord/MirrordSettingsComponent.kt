1c9605346cbb5e8a55e9464f963ede2f2f81811b
infiniteregrets <aroram18@mcmaster.ca>
2023-07-08 01:25:57 -0400

    .

--- modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordSettingsComponent.kt
+++ modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordSettingsComponent.kt
@@ -24,7 +24,7 @@ class MirrordSettingsComponent {
             versionCheckEnabled.isEnabled = telemetryEnabled.isSelected
             if (!telemetryEnabled.isSelected) {
                 versionCheckEnabled.isSelected = false
-            } 
+            }
         }
     }
 
