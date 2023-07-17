0b9e88fd8365f9f1345f2fb5868a965543a70f32
Razz4780 <msmolarekg@gmail.com>
2023-07-17 10:58:17 +0200

    Richer notification when active config is moved

--- modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordActiveConfigWatch.kt
+++ modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordActiveConfigWatch.kt
@@ -8,26 +8,36 @@ import com.intellij.openapi.vfs.newvfs.events.VFileMoveEvent
 
 class MirrordActiveConfigWatch(private val service: MirrordProjectService) : AsyncFileListener {
     override fun prepareChange(events: MutableList<out VFileEvent>): AsyncFileListener.ChangeApplier? {
-        val removeActive = service.activeConfig?.url?.let { activeUrl ->
-            events
-                .filter { it is VFileDeleteEvent || it is VFileMoveEvent }
-                .any { it.file?.url == activeUrl }
-        } ?: false
+        return service
+            .activeConfig
+            ?.url
+            ?.let { activeUrl ->
+                events
+                    .filter { it.file?.url == activeUrl }
+                    .firstNotNullOf {
+                        when (it) {
+                            is VFileDeleteEvent -> service.notifier.notification(
+                                "mirrord active config has been removed",
+                                NotificationType.WARNING
+                            )
+                                .withDontShowAgain(MirrordSettingsState.NotificationId.ACTIVE_CONFIG_REMOVED)
 
-        return if (removeActive) {
-            object : AsyncFileListener.ChangeApplier {
-                override fun afterVfsChange() {
-                    service.activeConfig = null
-                    service.notifier.notification(
-                        "mirrord active config has been removed",
-                        NotificationType.WARNING
-                    )
-                        .withDontShowAgain(MirrordSettingsState.NotificationId.ACTIVE_CONFIG_REMOVED)
-                        .fire()
+                            is VFileMoveEvent -> service.notifier.notification(
+                                "mirrord active config has been moved to ${it.newParent.presentableUrl}",
+                                NotificationType.WARNING
+                            )
+                                .withDontShowAgain(MirrordSettingsState.NotificationId.ACTIVE_CONFIG_REMOVED)
+
+                            else -> null
+                        }
+                    }
+            }?.let {
+                object : AsyncFileListener.ChangeApplier {
+                    override fun afterVfsChange() {
+                        service.activeConfig = null
+                        it.fire()
+                    }
                 }
             }
-        } else {
-            null
-        }
     }
 }
