578912b3d4db0e182d59bc50dd4fabd20fda6c79
infiniteregrets <aroram18@mcmaster.ca>
2023-08-29 10:03:01 -0400

    Make mirrord state more clear

--- modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordEnabler.kt
+++ modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordEnabler.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.DumbAware
+import icons.MirrordIcons
 
 class MirrordEnabler : ToggleAction(), DumbAware {
     override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
@@ -16,6 +17,7 @@ class MirrordEnabler : ToggleAction(), DumbAware {
     override fun setSelected(e: AnActionEvent, state: Boolean) {
         val project = e.project ?: throw Error("mirrord requires an open project")
         project.service<MirrordProjectService>().enabled = state
+        e.presentation.icon = if (state) MirrordIcons.enabled else MirrordIcons.disabled
     }
 
     override fun update(e: AnActionEvent) {
