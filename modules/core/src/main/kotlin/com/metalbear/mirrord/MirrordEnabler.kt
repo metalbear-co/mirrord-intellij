f541b08b6621ed63577751cffeabf80dac5fa371
infiniteregrets <aroram18@mcmaster.ca>
2023-09-15 11:15:50 -0400

    ..

--- modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordEnabler.kt
+++ modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordEnabler.kt
@@ -11,14 +11,13 @@ class MirrordEnabler : ToggleAction(), DumbAware {
     override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
     override fun isSelected(e: AnActionEvent): Boolean {
-        val state = e.project?.service<MirrordProjectService>()?.enabled ?: false
-        e.presentation.icon = if (state) MirrordIcons.enabled else MirrordIcons.disabled
-        return state
+        return e.project?.service<MirrordProjectService>()?.enabled ?: false
     }
 
     override fun setSelected(e: AnActionEvent, state: Boolean) {
         val project = e.project ?: throw Error("mirrord requires an open project")
         project.service<MirrordProjectService>().enabled = state
+        e.presentation.icon = if (state) MirrordIcons.enabled else MirrordIcons.disabled
     }
 
     override fun update(e: AnActionEvent) {
