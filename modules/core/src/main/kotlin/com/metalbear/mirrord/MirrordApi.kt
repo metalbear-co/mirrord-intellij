5c5d6d75293a2712ff7c7eeb505feebc6466eb06
meowjesty <meowjesty_nyan@outlook.com>
2023-09-15 15:36:48 -0300

    less formatting

--- modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordApi.kt
+++ modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordApi.kt
@@ -15,7 +15,11 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
-import java.util.concurrent.*
+import java.util.concurrent.CancellationException
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 enum class MessageType {
     NewTask,
