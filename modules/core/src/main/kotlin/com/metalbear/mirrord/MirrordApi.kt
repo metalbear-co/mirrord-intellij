deedf2a8d6847270f0f26fee641fc5de9956968c
meowjesty <meowjesty_nyan@outlook.com>
2023-10-02 16:56:50 -0300

    Change verify config action to pass --ide, changing some target related errors to warnings.

--- modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordApi.kt
+++ modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordApi.kt
@@ -184,7 +184,7 @@ class MirrordApi(private val service: MirrordProjectService) {
      * Reads the output (json) from stdout which contain either a success + warnings, or the errors from the verify
      * command.
      */
-    private class MirrordVerifyConfigTask(cli: String, path: String) : MirrordCliTask<String>(cli, "verify-config", "$path") {
+    private class MirrordVerifyConfigTask(cli: String, path: String) : MirrordCliTask<String>(cli, "verify-config", mapOf("--ide" to "", "--path" to "$path")) {
         override fun compute(project: Project, process: Process, setText: (String) -> Unit): String {
             setText("mirrord is verifying the config options...")
             process.waitFor()
@@ -270,8 +270,10 @@ class MirrordApi(private val service: MirrordProjectService) {
 
 /**
  * A mirrord CLI invocation.
+ *
+ * @param args: An extra list of arguments (used by `verify-config`).
  */
-private abstract class MirrordCliTask<T>(private val cli: String, private val command: String, private val args: String?) {
+private abstract class MirrordCliTask<T>(private val cli: String, private val command: String, private val args: Map<String, String>?) {
     var target: String? = null
     var configFile: String? = null
     var executable: String? = null
@@ -310,8 +312,11 @@ private abstract class MirrordCliTask<T>(private val cli: String, private val co
                 addParameter(it)
             }
 
-            args?.let {
-                addParameter(it)
+            args?.let { argsMap ->
+                argsMap.forEach {
+                    addParameter(it.key)
+                    addParameter(it.value)
+                }
             }
 
             environment["MIRRORD_PROGRESS_MODE"] = "json"
