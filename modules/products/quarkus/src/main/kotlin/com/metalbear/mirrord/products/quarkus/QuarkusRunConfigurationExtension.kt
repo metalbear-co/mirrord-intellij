183750669c35047836697f9df5fcf4c00059ebf1
meowjesty <meowjesty_nyan@outlook.com>
2023-10-23 18:31:16 -0300

    clear implementation

--- modules/products/quarkus/src/main/kotlin/com/metalbear/mirrord/products/quarkus/QuarkusRunConfigurationExtension.kt
+++ modules/products/quarkus/src/main/kotlin/com/metalbear/mirrord/products/quarkus/QuarkusRunConfigurationExtension.kt
@@ -6,16 +6,8 @@ import com.intellij.execution.RunConfigurationExtension
 import com.intellij.execution.configurations.JavaParameters
 import com.intellij.execution.configurations.RunConfigurationBase
 import com.intellij.execution.configurations.RunnerSettings
-import com.intellij.execution.process.ProcessEvent
 import com.intellij.execution.process.ProcessHandler
-import com.intellij.execution.process.ProcessListener
-import com.intellij.execution.target.createEnvironmentRequest
-import com.intellij.execution.wsl.target.WslTargetEnvironmentRequest
-import com.intellij.openapi.components.service
-import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Key
-import com.metalbear.mirrord.*
 import java.util.concurrent.ConcurrentHashMap
 
 class QuarkusRunConfigurationExtension : RunConfigurationExtension() {
@@ -25,13 +17,7 @@ class QuarkusRunConfigurationExtension : RunConfigurationExtension() {
     private val runningProcessEnvs = ConcurrentHashMap<Project, Map<String, String>>()
 
     override fun isApplicableFor(configuration: RunConfigurationBase<*>): Boolean {
-        val applicable = !configuration.name.startsWith("Build ")
-
-        if (!applicable) {
-            MirrordLogger.logger.info("Configuration name %s ignored".format(configuration.name))
-        }
-
-        return applicable
+        true
     }
 
     override fun isEnabledFor(
@@ -42,42 +28,14 @@ class QuarkusRunConfigurationExtension : RunConfigurationExtension() {
     }
 
     private fun <T : RunConfigurationBase<*>> getMirrordConfigPath(configuration: T, params: JavaParameters): String? {
-        return params.env[CONFIG_ENV_NAME] ?: if (configuration is ExternalSystemRunConfiguration) {
-            val ext = configuration as ExternalSystemRunConfiguration
-            ext.settings.env[CONFIG_ENV_NAME]
-        } else {
-            null
-        }
+        return null
     }
 
     override fun <T : RunConfigurationBase<*>> updateJavaParameters(
         configuration: T,
         params: JavaParameters,
         runnerSettings: RunnerSettings?
-    ) {
-        val service = configuration.project.service<MirrordProjectService>()
-
-        MirrordLogger.logger.debug("wsl check")
-        val wsl = when (val request = createEnvironmentRequest(configuration, configuration.project)) {
-            is WslTargetEnvironmentRequest -> request.configuration.distribution!!
-            else -> null
-        }
-
-        val mirrordEnv = service.execManager.wrapper("idea").apply {
-            this.wsl = wsl
-            configFromEnv = getMirrordConfigPath(configuration, params)
-        }.start()?.first?.let { it + mapOf(Pair("MIRRORD_DETECT_DEBUGGER_PORT", "javaagent")) }.orEmpty()
-
-        params.env = params.env + mirrordEnv
-        runningProcessEnvs[configuration.project] = params.env.toMap()
-
-        // Gradle support (and external system configuration)
-        if (configuration is ExternalSystemRunConfiguration) {
-            runningProcessEnvs[configuration.project] = configuration.settings.env.toMap()
-            configuration.settings.env = configuration.settings.env + mirrordEnv
-        }
-        MirrordLogger.logger.debug("setting env and finishing")
-    }
+    ) { }
 
     /**
      * Remove mirrord env leftovers from the external system configurations.
@@ -86,22 +44,5 @@ class QuarkusRunConfigurationExtension : RunConfigurationExtension() {
         configuration: RunConfigurationBase<*>,
         handler: ProcessHandler,
         runnerSettings: RunnerSettings?
-    ) {
-        if (configuration is ExternalSystemRunConfiguration) {
-            val envsToRestore = runningProcessEnvs.remove(configuration.project) ?: return
-
-            handler.addProcessListener(object : ProcessListener {
-                override fun processTerminated(event: ProcessEvent) {
-                    configuration.settings.env.apply {
-                        clear()
-                        putAll(envsToRestore)
-                    }
-                }
-
-                override fun startNotified(event: ProcessEvent) {}
-
-                override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {}
-            })
-        }
-    }
+    ) { }
 }
