52da4d55a57dba218c58f9d3d4c525dd0df8c737
Razz4780 <msmolarekg@gmail.com>
2024-10-08 11:24:19 +0200

    Checking DLV version in Goland config extension

--- modules/products/goland/src/main/kotlin/com/metalbear/mirrord/products/goland/GolandRunConfigurationExtension.kt
+++ modules/products/goland/src/main/kotlin/com/metalbear/mirrord/products/goland/GolandRunConfigurationExtension.kt
@@ -1,5 +1,6 @@
 package com.metalbear.mirrord.products.goland
 
+import com.github.zafarkhaja.semver.Version
 import com.goide.execution.GoRunConfigurationBase
 import com.goide.execution.GoRunningState
 import com.goide.execution.extension.GoRunConfigurationExtension
@@ -10,6 +11,7 @@ import com.intellij.execution.target.TargetedCommandLineBuilder
 import com.intellij.execution.wsl.target.WslTargetEnvironmentRequest
 import com.intellij.openapi.components.service
 import com.intellij.openapi.util.SystemInfo
+import com.metalbear.mirrord.MirrordLogger
 import com.metalbear.mirrord.MirrordPathManager
 import com.metalbear.mirrord.MirrordProjectService
 import java.nio.file.Paths
@@ -87,13 +89,40 @@ class GolandRunConfigurationExtension : GoRunConfigurationExtension() {
                 if (!delveExecutable.canExecute()) {
                     delveExecutable.setExecutable(true)
                 }
+
+                val ourDelveVersion = try {
+                    getDelveVersion(delvePath)
+                } catch (e: Throwable) {
+                    MirrordLogger.logger.error("Failed to get version of our bundled DLV $delvePath", e)
+                    null
+                }
+
                 // parameters returns a copy, and we can't modify it so need to reset it.
                 val patchedParameters = executor.parameters
                 for (i in 0 until patchedParameters.size) {
                     // no way to reset the whole commandline, so we just remove each entry.
                     executor.withoutParameter(0)
-                    val value = patchedParameters[i]
-                    if (value.toPresentableString().endsWith("/dlv", true)) {
+
+                    val foundDelvePath = patchedParameters[i].toPresentableString()
+
+                    if (!foundDelvePath.endsWith("/dlv", true)) {
+                        continue
+                    }
+
+                    val delveVersion = if (ourDelveVersion == null) {
+                        null
+                    } else {
+                        try {
+                            getDelveVersion(foundDelvePath)
+                        } catch (e: Throwable) {
+                            MirrordLogger
+                                .logger
+                                .error("Failed to get version of DLV found in executor parameters $foundDelvePath", e)
+                            null
+                        }
+                    }
+
+                    if (delveVersion == null || delveVersion.lessThan(ourDelveVersion)) {
                         patchedParameters[i] = PathParameter(delveExecutable.toString())
                     }
                 }
@@ -105,5 +134,25 @@ class GolandRunConfigurationExtension : GoRunConfigurationExtension() {
 
     private fun getCustomDelvePath(): String {
         return MirrordPathManager.getBinary("dlv", false)!!
+    }
+
+    private fun getDelveVersion(path: String): Version {
+        val process = ProcessBuilder(path, "version")
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .redirectError(ProcessBuilder.Redirect.DISCARD)
+            .start()
+
+        val exitCode = process.waitFor()
+        if (exitCode != 0) {
+            throw Exception("dlv version command exited with code $exitCode")
+        }
+
+        val versionLine = process
+            .inputStream
+            .bufferedReader()
+            .readLines()
+            .find { it.startsWith("Version: ") } ?: throw Exception("no output line starts with 'Version: '")
+
+        return Version.valueOf(versionLine.removePrefix("Version: "))
     }
 }
