2c9e93b2fb3e5987b4b22ba596e9ea2dcf810b96
Gemma Tipper <gemmat@metalbear.co>
2025-06-19 09:08:28 +0100

    Exception handling

--- modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordApi.kt
+++ modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordApi.kt
@@ -611,14 +611,18 @@ private abstract class MirrordCliTask<T>(private val cli: String, private val co
                 val process =
                     Runtime.getRuntime().exec(arrayOf("git", "-C", it.canonicalPath, "branch", "--show-current"))
 
-                if (process.waitFor(10, TimeUnit.SECONDS) && process.exitValue() == 0) {
-                    val branchName = process.inputStream.bufferedReader().use { it.readText() }.trim()
-                    if (branchName.isNotEmpty()) {
-                        environment["MIRRORD_BRANCH_NAME"] = branchName
+                try {
+                    if (process.waitFor(10, TimeUnit.SECONDS) && process.exitValue() == 0) {
+                        val branchName = process.inputStream.bufferedReader().use { it.readText() }.trim()
+                        if (branchName.isNotEmpty()) {
+                            environment["MIRRORD_BRANCH_NAME"] = branchName
+                        }
+                    } else {
+                        val stderrOutput = process.errorStream.bufferedReader().use { it.readText() }
+                        MirrordLogger.logger.debug("error retrieving git branch: $stderrOutput")
                     }
-                } else {
-                    val stderrOutput = process.errorStream.bufferedReader().use { it.readText() }
-                    MirrordLogger.logger.debug("error retrieving git branch: $stderrOutput")
+                } catch (e: Exception) {
+                    MirrordLogger.logger.debug("exception while running git command, Jira integration metrics will not be recorded: $e")
                 }
             }
 
