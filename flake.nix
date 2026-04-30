fa1a1f1945638360d23e49fe77a3a78c49ce3dc0
Vinicius Deolindo <viniciusd@metalbear.com>
2026-04-30 12:48:07 -0300

    Add a nix devshell (#428)

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            jdk17
+            jetbrains.idea
+          ];
+        };
+      }
+    );
+}
