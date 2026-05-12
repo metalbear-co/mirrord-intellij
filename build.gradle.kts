4de77cb54ec7b48bbfbb78a30d85cb75af05a9f3
Vinicius Deolindo <viniciusd@metalbear.com>
2026-05-12 16:58:19 -0300

    Disable code instrumentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,6 +111,10 @@ changelog {
 }
 
 intellijPlatform {
+    // RubyMine 2024.1 does not publish the java-compiler-ant-tasks artifact needed by IntelliJ code instrumentation.
+    // We do not use IntelliJ GUI Designer .form files or Java sources, so instrumentation is unnecessary.
+    instrumentCode.set(false)
+
     pluginConfiguration {
         version = providers.gradleProperty("pluginVersion")
 
